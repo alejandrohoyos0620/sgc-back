f0884ad91487e53e864b7cf3cefd0f9a8a967c99
Manuel Narvaez <manuel260497@gmail.com>
2021-03-16 08:15:13 -0500

    Corrección en metodo de HTTP Request

--- routes/api/authorization.js
+++ routes/api/authorization.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AuthorizationService = require('../../services/authorization');
 const authService = new AuthorizationService();
 
-router.get('/', async function(req, res, next){
+router.post('/', async function(req, res, next){
     try {
         const result = await authService.login(req.body);
         res.status(result[0]).json(result[1]);
